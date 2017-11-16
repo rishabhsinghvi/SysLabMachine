c0dcae2347262e7a0a4a4c04e87e80a9a106c70c
peterstan <p.stanwicks@gmail.com>
2017-11-15 23:55:41 -0500

    test on quark p

--- sim-mips.c
+++ sim-mips.c
@@ -648,13 +648,13 @@ struct inst parser(char *input){
 	commandArgs[argumentCount][column] = '\0';
 	argumentCount++;
 	
-	/*
+	
 	int v = 0;
 	for (v = 0; v < argumentCount; v++){
 		printf("TEMPARGS[%s]\n",commandArgs[v]);
 		
 	}
-	*/
+	
 	
 	
 	
